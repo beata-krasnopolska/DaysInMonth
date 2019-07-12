62ba831437c57b7c3ba0878998fc240b8007f588
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-07-12 21:59:58 +0200

    Add project files.

---
+++ DaysInMonth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace DaysInMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program gets the number of days of a given month for a year");
+
+            Console.WriteLine("Enter the month number: ");
+            int monthNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the year number: ");
+            int year = int.Parse(Console.ReadLine());
+
+            DateTimeFormatInfo dateInfo = new DateTimeFormatInfo();
+            string monthName = dateInfo.GetMonthName(monthNumber);
+            int daysInMonth = DateTime.DaysInMonth(year, monthNumber);
+
+            Console.WriteLine("The number of days in the month {0} is: {1}", monthName, daysInMonth);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DaysInMonth/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("DaysInMonth")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DaysInMonth")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("dedcd73f-0e5b-4d36-bd32-25434c9154b9")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
